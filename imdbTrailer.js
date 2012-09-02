dc2acf7dfdccdeddb751fb98fb8ca6395fe7aa7e
Kalen Krempely <kalensk@gmail.com>
2012-09-02 16:35:51 -0700

    v1.2
    Find trailer link works by seraching google video to returning top
    result works

--- imdbTrailer.js
+++ imdbTrailer.js
@@ -3,16 +3,21 @@
 	
 //Extract Title and Year from imdb
 //var movieTitle = document.title.slice(0,-14);	
-var movieTitle = $('h1').text().slice(0,-10);
+var movieTitle = $('h1').text().slice(0,-18);
+var movieTitleLink = encodeURI('"' + movieTitle.slice(1) + '"');
+
 var movieYear = $('h1.header a[href*=year]').text();
+
 
 	
 //If 'Watch Trailer' link is not present, then create & insert 'Find Trailer' link
 
 // if(!($(".title-trailer").length)){
 if(($(".title-trailer").length)===0){
-	var TrailerLink = "http://www.youtube.com/";
+	var TrailerLink = "http://www.google.com/search?q=" + movieTitleLink + " " + movieYear + "%20trailer&tbm=vid&tbs=dur:s&btnI";
 
+	
+	
     $('<a href="' + TrailerLink + '" class="btn2 large btn2_text_on title-trailer" <span class="btn2_text">Find Trailer</span></a>').insertBefore(".launch-share-popover");
 }
 
