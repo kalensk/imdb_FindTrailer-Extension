b1340e0bc42d04291c5129680b930094a5a77d6b
Kalen Krempely <kalensk@gmail.com>
2012-09-03 00:13:24 -0700

    v1.2

--- imdbTrailer.js
+++ imdbTrailer.js
@@ -1,24 +1,12 @@
-                               
-//scrape movie title and year to search youtube
-	
-//Extract Title and Year from imdb
-//var movieTitle = document.title.slice(0,-14);	
-var movieTitle = $('h1').text().slice(0,-18);
-var movieTitleLink = encodeURI('"' + movieTitle.slice(1) + '"');
 
-var movieYear = $('h1.header a[href*=year]').text();
-
-
-	
 //If 'Watch Trailer' link is not present, then create & insert 'Find Trailer' link
-
-// if(!($(".title-trailer").length)){
 if(($(".title-trailer").length)===0){
-	var TrailerLink = "http://www.google.com/search?q=" + movieTitleLink + " " + movieYear + "%20trailer&tbm=vid&tbs=dur:s&btnI";
 
+	//Extract Title and Year from imdb
+	var movieTitle = encodeURI('"' + document.title.slice(0,-14).replace(/ /g,"+") + '"');
+	var movieYear = $('h1.header a[href*=year]').text();
 	
-	
-    $('<a href="' + TrailerLink + '" class="btn2 large btn2_text_on title-trailer" <span class="btn2_text">Find Trailer</span></a>').insertBefore(".launch-share-popover");
+	var TrailerLink = "http://www.google.com/search?q=" + movieTitle + "+" + movieYear + "+trailer&tbm=vid&tbs=dur:s&btnI";
+
+	$('<a href="' + TrailerLink + '" class="btn2 large btn2_text_on title-trailer" <span class="btn2_text">Find Trailer</span></a>').insertBefore(".launch-share-popover");
 }
-
-
