acde440af94fe5d2da87bf7cf4314cd6b3929ead
Kalen Krempely <kalensk@gmail.com>
2012-09-03 22:43:20 -0700

    v1.3 - colorbox
    Added colorbox support, but errors on rendering. Possible conflict with
    imdb's CSS....??

--- imdbTrailer.js
+++ imdbTrailer.js
@@ -1,12 +1,18 @@
+//if 'Watch Trailer' link is not present, then insert 'Find Trailer' link
+jQuery(document).ready(function($) {
+	if(($(".title-trailer").length)===0){
 
-//If 'Watch Trailer' link is not present, then create & insert 'Find Trailer' link
-if(($(".title-trailer").length)===0){
-
-	//Extract Title and Year from imdb
-	var movieTitle = encodeURI('"' + document.title.slice(0,-14).replace(/ /g,"+") + '"');
-	var movieYear = $('h1.header a[href*=year]').text();
-	
-	var TrailerLink = "http://www.google.com/search?q=" + movieTitle + "+" + movieYear + "+trailer&tbm=vid&tbs=dur:s&btnI";
-
-	$('<a href="' + TrailerLink + '" class="btn2 large btn2_text_on title-trailer" <span class="btn2_text">Find Trailer</span></a>').insertBefore(".launch-share-popover");
-}
+		//extract Title and Year from imdb
+		var movieTitle = encodeURI('"' + document.title.slice(0,-14).replace(/ /g,"+") + '"');
+		var movieYear = $('h1.header a[href*=year]').text();
+		
+		//create a link to search google and return top result using MovieTitle and MovieYear
+		var TrailerLink = "http://www.google.com/search?q=" + movieTitle + "+" + movieYear + "+trailer&tbm=vid&tbs=dur:s&btnI";
+		
+		//find 'Share...' button on imdb and insert 'Find Trailer' button before it
+		$('<a href="' + TrailerLink + '" class="btn2 large btn2_text_on title-trailer" <span class="btn2_text">Find Trailer</span></a>').insertBefore(".launch-share-popover");
+		
+		//render colorbox popup to display TrailerLink
+		$("a.title-trailer").colorbox({iframe:true, width:"70%", height:"70%"});
+	}
+});
